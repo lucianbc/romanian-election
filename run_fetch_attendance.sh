a9dff0a7e6b3d9a8821a0a199d02ab6acd00d915
lucianbc <lucianboaca97@gmail.com>
2025-05-18 14:36:33 +0100

    Update attendance

--- run_fetch_attendance.sh
+++ run_fetch_attendance.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-cd /Users/lucianboaca/Documents/Code/romania-election-views
-node scripts/fetch_attendance.js
-git add .
-git commit -m "Update attendance"
-git push
+while true  
+do  
+  echo "Running update"
+  node scripts/fetch_attendance.js
+  git add .
+  git commit -m "Update attendance"
+  git push
+  sleep 300  
+done
